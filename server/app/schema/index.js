3f9f3470d84317d05215e33d78b92ed126df3d57
Vadim <webdevelopers52@gmail.com>
2018-02-01 21:40:51 +0200

    refactor delete resolvers file

--- server/app/schema/index.js
+++ server/app/schema/index.js
@@ -8,5 +8,9 @@ module.exports = makeExecutableSchema({
 		require('./tag/tag.type')
 	],
 
-	resolvers: require('./resolvers')
+	resolvers: {
+		Query: require('./root/root.resolvers'),
+		User: require('./user/user.resolvers'),
+		Article: require('./article/article.resolvers'),
+	}
 });
--- server/app/schema/resolvers.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	Query: require('./root/root.resolvers'),
-	User: require('./user/user.resolvers'),
-	Article: require('./article/article.resolvers'),
-};

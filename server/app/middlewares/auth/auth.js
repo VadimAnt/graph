b6cf0fca57a8137632692a1d5e77e3b4c769c8ab
Vadim <webdevelopers52@gmail.com>
2018-02-10 13:54:12 +0200

    feat: added authService userService

--- server/app/middlewares/auth/auth.js
+++ server/app/middlewares/auth/auth.js
@@ -1,13 +1,13 @@
 const { AuthService, UserService } = require('../../services');
 
 const authService = new AuthService();
+const userService = new UserService();
 
 module.exports = {
 
 	auth: async (req, res) => {
 		try {
 			const data = await authService.verify(req);
-			const userService = new UserService();
 			if (!await userService.initialise(req, data)) {
 				throw Error('User not found');
 			}
